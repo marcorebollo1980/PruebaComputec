e8b58ab8eae2e0e3f98dd3b19229b076779927a7
marcorebollo1980 <marcorebollolopez@gmail.com>
2021-11-28 13:40:11 -0500

    se agregan metodos
    delete y put

--- src/main/java/com/desarrolloPruebaComputec/co/prueba/controller/BookController.java
+++ src/main/java/com/desarrolloPruebaComputec/co/prueba/controller/BookController.java
@@ -4,9 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,6 +40,29 @@ public class BookController {
 	   return bookRepository.findById(id);	
 	}
 	
+	@PutMapping("/updateBook/{id}")
+	public ResponseEntity<Book> updateTutorial(@PathVariable("id") int id, @RequestBody Book book) {
+	  Optional<Book> bookData = bookRepository.findById(id);
+
+	  if (bookRepository != null) {
+	    Book b = bookData.get();
+	    b.setBookName(book.getBookName());
+	    b.setAuthorName(book.getAuthorName());
+	    return new ResponseEntity<>(bookRepository.save(b), HttpStatus.OK);
+	  } else {
+	    return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	  }
+	}
+	
+	@DeleteMapping("/deleteBook/{id}")
+	public ResponseEntity<HttpStatus> deleteBook(@PathVariable("id") int id) {
+	  try {
+		  bookRepository.deleteById(id);
+	    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	  } catch (Exception e) {
+	    return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+	  }
+	}
 	
 	
 	
